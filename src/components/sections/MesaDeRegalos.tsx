6642199e9524d6f1779293e039c403064023e5a5
Enrique <enrique_alvarado95@hotmail.com>
2024-02-26 10:59:07 -0600

    se agrega datos de transferencia bancaria

--- src/components/sections/MesaDeRegalos.tsx
+++ src/components/sections/MesaDeRegalos.tsx
@@ -5,10 +5,12 @@ import liverpool from "../../../public/assets/images/liverpool.webp";
 import amazon from "../../../public/assets/images/amazon_logo-removebg-preview.png";
 import Button from "../UI/Button";
 import AnimatedTitle from "../UI/AnimatedTitle";
-
+import { inter } from "@/fonts";
+// Daniela Vazquez Gaytan
+// 4152 3141 9031 7340 BBVA
 const MesaDeRegalos = () => {
   return (
-    <div className="h-[300px] flex flex-col items-center justify-center cell:h-auto cell:py-4">
+    <div className=" flex flex-col items-center justify-center h-auto py-4">
       <AnimatedTitle title="Sugerencia de regalos" />
       <div className="flex justify-evenly items-center py-4 gap-4 w-[60%] mx-auto cell:flex-col ">
         <div className=" w-1/3 cell:w-11/12 cell:mx-auto cell:flex cell:justify-center">
@@ -28,6 +30,13 @@ const MesaDeRegalos = () => {
             text="Ver Mesa"
           />
         </div>
+      </div>
+      <div className=" text-center text-3xl mx-auto my-6 border border-slate-700 rounded-md shadow-md py-4 px-7 cell:text-2xl cell:mx-2">
+        <p>Transferencia Bancaria</p>
+        <p> Daniela Vazquez Gaytan </p>
+        <b className={`${inter.className} text-2xl`}>
+          4152 3141 9031 7340 BBVA
+        </b>
       </div>
     </div>
   );
