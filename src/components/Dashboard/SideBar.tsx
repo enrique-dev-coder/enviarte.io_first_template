c46f78ef7f5b0a3d1e0a26b096ddd00f9f008bac
Enrique <enrique_alvarado95@hotmail.com>
2024-03-12 13:00:18 -0600

    se agrega una Key que faltaba

--- src/components/Dashboard/SideBar.tsx
+++ src/components/Dashboard/SideBar.tsx
@@ -35,7 +35,7 @@ const SideBar = () => {
       <div className="my-2">
         {menuLinks.map((menuItem) => {
           return (
-            <Link href={menuItem.link} id={menuItem.id}>
+            <Link href={menuItem.link} key={menuItem.id}>
               <div
                 className={`flex px-3 py-2 items-center gap-2 cursor-pointer
             ${currentPath === menuItem.link && " bg-purple-950 text-white"}
