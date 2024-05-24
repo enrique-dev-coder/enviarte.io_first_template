0f227d87edfb0a9028c3bb49b305c217c1bda94a
Enrique <enrique_alvarado95@hotmail.com>
2024-05-23 21:57:52 -0600

    fix

--- src/app/InvitationTemplates/TemplateComponents/Simplified/Itinerario.tsx
+++ src/app/InvitationTemplates/TemplateComponents/Simplified/Itinerario.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import AnimatedTitle from "../UI/AnimatedTitle";
+import AnimatedTitle from "./AnimatedTitle";
 import { lora } from "@/fonts";
 
 interface ActividadProps {
