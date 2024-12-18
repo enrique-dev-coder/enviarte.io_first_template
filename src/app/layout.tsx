18d34114e1e0f674cf2f31e4f65e4387203cb646
enrique-dev-coder <enrique_alvarado95@hotmail.com>
2024-12-18 11:55:15 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,25 @@ import "./globals.css";
 import { inter } from "@/fonts";
 import ReactQueryContainer from "@/components/wrappers/ReactQueryContainer";
 
-export const metadata: Metadata = {
-  title: "InvitandoFacil",
+export const metadata = {
+  metadataBase: new URL("https://www.invitandofacil.com"),
+  title: "Mica & Rosy",
   description: "Invitaciones digitales para eventos",
+  openGraph: {
+    siteName: "Mica & Rosy ",
+    title: "Mica & Rosy - Nuestra Boda",
+    description:
+      "Te invitamos a celebrar nuestra boda. Haz clic para más detalles.",
+    url: "https://www.invitandofacil.com/boda/mica&rosy",
+    images: [
+      {
+        url: "https://www.invitandofacil.com/assets/images/micayrosy/Fut.jpg",
+        width: 256,
+        height: 256,
+        alt: "Imagen de la boda de Mica y Rosy",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
