046e17d469d58d0d28929b4726de006aaa342571
enrique-dev-coder <enrique_alvarado95@hotmail.com>
2024-12-18 11:23:29 -0600

    probando etiquetas opengraph

--- src/app/boda/mica&rosy/page.tsx
+++ src/app/boda/mica&rosy/page.tsx
@@ -3,9 +3,24 @@ import type { Metadata } from "next";
 
 import Body from "./Body";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Mica & Rosy",
   description: "Invitaciones digitales para eventos",
+  openGraph: {
+    title: "Mica & Rosy - Nuestra Boda",
+    description:
+      "Te invitamos a celebrar nuestra boda. Haz clic para más detalles.",
+    url: "https://www.invitandofacil.com/boda/mica&rosy",
+    images: [
+      {
+        url: "https://www.invitandofacil.com/assets/images/micayrosy/Fut.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Imagen de la boda de Mica y Rosy",
+      },
+    ],
+    siteName: "Invitando Fácil",
+  },
 };
 
 const page = () => {
