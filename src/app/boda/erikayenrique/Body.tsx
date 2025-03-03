8c311cf861278207edb7f16fee8c458d14fad470
propiedadessaltillo <71940372+enrique-dev-coder@users.noreply.github.com>
2025-03-02 20:41:15 -0600

    invitacion erika y enrique

---
+++ src/app/boda/erikayenrique/Body.tsx
@@ -0,0 +1,385 @@
+"use client";
+import React from "react";
+import InvitationContainer from "@/components/wrappers/InvitationContainer";
+import IngresarBoton from "@/app/InvitationTemplates/TemplateComponents/Customized/CommonComponents/IngresarBoton";
+import PlaceCard from "@/app/InvitationTemplates/TemplateComponents/Modern/PlaceCard";
+import MesaDeRegalos from "@/app/InvitationTemplates/TemplateComponents/Customized/CommonComponents/MesaDeRegalos";
+import SongButtonPlayer from "@/app/InvitationTemplates/Common/SongButtonPlayer";
+import ItinerarioScrollable from "@/app/InvitationTemplates/TemplateComponents/Customized/Itinerarios/ItinerarioScrollable";
+
+import { lora, champagne, dancing } from "@/fonts";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+import Hero from "./Hero";
+import Contador from "./Contador";
+//images
+import imgBanner from "/public/assets/images/erikayenrique/IMG_0243.png";
+import img3 from "/public/assets/images/erikayenrique/IMG_0247.png";
+import img5 from "/public/assets/images/erikayenrique/IMG_0249.png";
+import img6 from "/public/assets/images/erikayenrique/IMG_0251.png";
+import img7 from "/public/assets/images/erikayenrique/IMG_0253.png";
+import img8 from "/public/assets/images/erikayenrique/IMG_0254.png";
+import img9 from "/public/assets/images/erikayenrique/IMG_0257.png";
+import img10 from "/public/assets/images/erikayenrique/IMG_0258.png";
+
+import { WhatsIcon } from "@/components/Dashboard/EnviarLista/Icons";
+import Sobre from "/public/assets/images/email.png";
+import HotelCard from "./HotelCard";
+import DressCodeMujeres from "/public/assets/images/vestidolargo.png";
+import DressCodeHombres from "/public/assets/images/suit_icon-removebg-preview.png";
+
+const textOlive = "text-[#7f5539]";
+const bgOlive = "bg-[#b08968]";
+// redeploy x2
+const Body = () => {
+  return (
+    <InvitationContainer
+      spBackground="none"
+      songLink="/assets/songs/idaliayricardo.mp3"
+    >
+      <IngresarBoton
+        buttonText="Ingresar"
+        font={dancing}
+        novia="Erika"
+        novio="Enrique"
+        join="y"
+        color={{
+          bgColor: bgOlive,
+          titleColor: textOlive,
+          textButtonColor: "text-white",
+        }}
+      />
+      <Hero />
+      <div className=" bg-white py-8">
+        <motion.h2
+          initial={{ opacity: 0, y: 5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ ease: "easeInOut", duration: 1 }}
+          viewport={{ once: true }}
+          className={` ${textOlive} text-center  text-[28px]    py-4 `}
+        >
+          Con la bendición de nuestros padres
+        </motion.h2>
+        <div>
+          <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
+            viewport={{ once: true }}
+            className="flex flex-col gap-1 w-[300px] cell:w-full bg-white  py-4 my-2 text-center cell:text-2xl cell:text-center"
+          >
+            <p className={`text-2xl font-bold ${textOlive}`}>
+              Papás de la novia
+            </p>
+            <p className={`${dancing.className} text-[28px]  font-medium`}>
+              Esperanza Pérez Esquivel
+            </p>
+            <p
+              className={`${dancing.className} text-[28px] text-gray-800 font-medium`}
+            >
+              y
+            </p>
+            <p
+              className={`${dancing.className} text-[28px] text-gray-800 font-medium`}
+            >
+              José Ángel Gámez Lozano
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
+            viewport={{ once: true }}
+            className="flex flex-col gap-1 my-2 w-[300px] cell:w-full bg-white py-4 text-center cell:text-2xl cell:text-center"
+          >
+            <p className={`text-2xl font-bold ${textOlive}`}>Papás del novio</p>
+            <p className={`${dancing.className} text-[28px]  font-medium`}>
+              María Del Socorro Sánchez Ríos
+            </p>
+            <p
+              className={`${dancing.className} text-[28px] text-gray-800 font-medium`}
+            >
+              y
+            </p>
+            <p
+              className={`${dancing.className} text-[28px] text-gray-800 font-medium`}
+            >
+              Juan Alberto Molina Galindo
+            </p>
+          </motion.div>
+        </div>
+      </div>
+      <Contador
+        fechaEvento={new Date("2025-04-04T00:00:00")}
+        fechaString="Viernes, 4 de Abril de 2025"
+        fotoContador="bg-[url('/assets/images/erikayenrique/date.png')]"
+      />
+      <PlaceCard
+        show={true}
+        titleColor={textOlive}
+        titleFont={dancing}
+        bgButtonColor={bgOlive}
+        desc={"Ceremonia Religiosa"}
+        direccion={
+          "Calle Gral. Manuel Pérez Treviño 425, Zona Centro, 25000 Saltillo, Coah."
+        }
+        hora={"5:00 pm "}
+        nombre={{
+          content: "Santuario de Guadalupe",
+          color: textOlive,
+          font: lora,
+        }}
+        ubicacion={"https://maps.app.goo.gl/71RHo1dBTJYxe1uX8"}
+      />
+      <PlaceCard
+        show={true}
+        titleColor={textOlive}
+        titleFont={dancing}
+        bgButtonColor={bgOlive}
+        desc={"Ceremonia Civil"}
+        direccion={"Eulalio Gutiérrez 381, La Palmilla, 25204 Saltillo, Coah."}
+        hora={"7:30 pm "}
+        nombre={{
+          content: "Quinta El Portal",
+          color: textOlive,
+          font: lora,
+        }}
+        ubicacion={"https://maps.app.goo.gl/WUu1MGNRqXnjTrNR9"}
+      />
+
+      <motion.div
+        initial={{ opacity: 0, y: 5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={imgBanner} alt="boda" />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img3} alt="boda" />
+      </motion.div>
+      <motion.h2
+        initial={{ opacity: 0, y: 5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+        className={`${dancing.className} text-center ${textOlive} font-bold   text-5xl cell:text-4xl py-4 `}
+      >
+        Itinerario
+      </motion.h2>
+      <ItinerarioScrollable
+        showItinerario={true}
+        eventData={[
+          {
+            id: 2,
+            evento: "Ceremonia Religiosa ",
+            hora: "5:00 pm",
+            Icon: "recepcion",
+          },
+          {
+            id: 1,
+            evento: `Ceremonia Civil`,
+            hora: "7:30 pm",
+            Icon: "church",
+          },
+          {
+            id: 7,
+            evento: "Entrada de novios",
+            hora: "9:00 pm",
+            Icon: "civil",
+          },
+          {
+            id: 4,
+            evento: " Cena",
+            hora: "9:30 pm",
+            Icon: "dinner",
+          },
+          {
+            id: 5,
+            evento: "Inicia la fiesta",
+            hora: "10:30 pm",
+            Icon: "party",
+          },
+          {
+            id: 6,
+            evento: "Cierre del evento",
+            hora: "1:30 am",
+            Icon: "flores",
+          },
+        ]}
+        sectionStyling={{
+          lineColor: bgOlive,
+          lineBg: "bg-[#b08968]/20",
+          circleColor: "stroke-[#b08968]",
+          circleBg: "bg-gray-50",
+          circleSoftStroke: "stroke-[#b08968]/20",
+          iconColor: "text-gray-900",
+          cardBg: "bg-white",
+        }}
+      />
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img6} alt="boda" />
+      </motion.div>
+
+      <MesaDeRegalos
+        FraseAgradecimientoFinalColor=""
+        FraseAgradecimientoFinalFont={undefined}
+        FraseAgradecimientoFinalSize=""
+        FraseAgradecimientoFinalText=""
+        FraseAgradecimientoInicioColor=""
+        FraseAgradecimientoInicioFont={undefined}
+        FraseAgradecimientoInicioSize=""
+        FraseAgradecimientoInicioText=""
+        SectionTitleColor={textOlive}
+        SectionTitleFont={dancing}
+        ShowFraseDeAgradecimientoFinal={false}
+        ShowFraseDeAgradecimientoInicio={false}
+        ShowRegalo={false}
+        ShowSobres={true}
+        ShowTransferencia={false}
+        datosTransferencia={{
+          numero: `Tarjeta Idalia de la Fuente\n 5579 0701 3285 9151 \n Clabe\n 014078606040656221`,
+          owner: "Cuenta Santander",
+        }}
+        SobresImg={Sobre}
+        SobresTextColor=""
+        SobresTextContent="Lluvia de sobres"
+        SobresType="modern"
+        backGround=""
+        ShowLiverpool={false}
+        liverpoolNoEvento="51556397"
+        showAmazon={false}
+        linkLiverpool="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51556397"
+      />
+
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img5} alt="boda" />
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img7} alt="boda" />
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img8} alt="boda" />
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img9} alt="boda" />
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image src={img10} alt="boda" />
+      </motion.div>
+      <motion.h2
+        initial={{ opacity: 0, y: 5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+        className={`${champagne.className} text-center ${textOlive} font-bold   text-5xl cell:text-4xl py-4 `}
+      >
+        Dress Code
+      </motion.h2>
+      <motion.h2
+        initial={{ opacity: 0, y: 5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+        className={`${lora.className} text-center ${textOlive} font-bold underline   text-5xl cell:text-4xl py-4 `}
+      >
+        Formal
+      </motion.h2>
+      <div className="w-6/12 flex flex-col justify-center items-center cell:w-full">
+        {" "}
+        <Image alt="boda" src={DressCodeMujeres} className="w-[50%]" />
+        <p className={`font-bold text-4xl ${textOlive} cell:text-2xl`}>
+          Mujeres
+        </p>
+        <p className="  text-4xl mb-4 cell:text-xl cell:mb-1">Vestido Largo</p>
+      </div>
+
+      <div className="w-6/12 flex flex-col justify-center items-center cell:w-full">
+        {" "}
+        <Image alt="boda" src={DressCodeHombres} className="w-[35%]" />
+        <p className={`font-bold text-4xl ${textOlive} cell:text-2xl`}>
+          Hombres
+        </p>
+        <p className="  text-4xl mb-4 cell:mb-1 cell:text-xl">Traje Formal</p>
+      </div>
+      <motion.h2
+        initial={{ opacity: 0, y: 5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+        viewport={{ once: true }}
+        className={`${lora.className} text-center ${textOlive} font-bold underline   text-5xl cell:text-4xl py-4 `}
+      >
+        NO NIÑOS
+      </motion.h2>
+      <div>
+        <motion.p
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ ease: "easeInOut", duration: 1 }}
+          viewport={{ once: true }}
+          className={`${textOlive} uppercase text-center ${champagne.className}  text-[40px]`}
+        >
+          Confirmación de Asistencia
+        </motion.p>
+      </div>
+      <div className="relative">
+        <div className="  py-2">
+          <div className="flex flex-col justify-center items-center py-2">
+            <p
+              className={`${lora.className}  text-4xl cell:text-2xl text-center font-bold`}
+            >
+              Wedding Planner <br /> Viviana Amare
+            </p>
+            <a href="https://api.whatsapp.com/send?phone=5218447006000">
+              <button
+                className={`${bgOlive} flex items-center gap-3  my-3 rounded-full px-4 py-2`}
+              >
+                <WhatsIcon className="fill-white text-2xl " />
+                <p className=" text-3xl text-white">844 700 6000</p>
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <SongButtonPlayer backgroundColor={bgOlive} />
+    </InvitationContainer>
+  );
+};
+
+export default Body;
---
+++ src/app/boda/erikayenrique/Hero.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useContext } from "react";
+import { StartAnimation } from "@/contexts/AnimationContext";
+import Image from "next/image";
+import { lora, champagne, dancing } from "@/fonts";
+import imgHero from "/public/assets/images/erikayenrique/intro.png";
+import imgLogo from "/public/assets/images/erikayenrique/logo.png";
+
+const Hero = () => {
+  const { controls } = useContext(StartAnimation);
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${imgHero.src})`,
+        }}
+        className=" h-screen  bg-cover bg-center bg-fixed  bg-no-repeat  bg-blend-overlay bg-black/10 "
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 5 }}
+          animate={controls}
+          transition={{ ease: "easeInOut", duration: 1 }}
+          className=" h-full flex flex-col justify-between items-center"
+        >
+          <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            transition={{ ease: "easeInOut", duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <Image
+              alt="boda"
+              src={imgLogo}
+              className=" w-[84%] bg-[#dcd3c4]   mx-auto "
+            />
+          </motion.div>
+          <div className="text-center">
+            <motion.div
+              initial={{ opacity: 0, y: 5 }}
+              animate={controls}
+              transition={{ ease: "easeInOut", duration: 1 }}
+              className={`${dancing.className}  text-white font-bold`}
+            >
+              <h1 className="  text-white font-semibold  text-[48px] leading-[40px] ">
+                Erika
+                <br />
+                <span>y</span>
+                <br />
+                Enrique
+              </h1>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 5 }}
+              animate={controls}
+              transition={{ ease: "easeInOut", duration: 1 }}
+              className={`${dancing.className} text-[32px] text-white font-bold pb-10`}
+            >
+              <h1 className="   text-white font-semibold  text-[32px] leading-[60px] ">
+                4 de Abril de 2025
+              </h1>
+            </motion.div>
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
