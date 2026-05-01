4d0d84e1a92d13327e305e7af82043fce8dc3a3f
propiedadessaltillo <enrique_alvarado95@hotmail.com>
2026-05-01 12:10:34 -0600

    Salvar imagenes

--- src/app/fiestas/anabell/Body.tsx
+++ src/app/fiestas/anabell/Body.tsx
@@ -7,18 +7,12 @@ import SongButtonPlayer2 from "@/app/InvitationTemplates/Common/SongButtonPlayer
 import { ClassicDolly, ModernDolly, RetroDolly, paris } from "@/fonts";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import Hero from "./Hero";
-import Contador from "./Contador";
 import { MapPin } from "lucide-react";
 
-const textOlive = "text-[#7A553A]";
-const invitationbg = "bg-[#fdeaf2]";
-const bgOlive = "bg-[#6b705c]";
+const invitationbg = "bg-[#F9E6ED]";
 const color1 = "text-[#d83a85]";
 const color2 = "text-[#fe9dc8]";
-const thirdColor = "text-[#6b705c]";
-const extraBg = "bg-[#f2ebe2]";
-
+import BolaDisco from "/public/assets/images/anabell/boladisco-removebg-preview.png";
 const Body = ({
   nombre,
   tel,
@@ -35,31 +29,84 @@ const Body = ({
       spBackground="none"
       songLink="/assets/songs/anabell.mp3"
     >
-      {/* <IngresarBoton
-        buttonText="Ingresar"
-        bgCuadrito="bg-white"
-        extraClases=" !mt-[16px] !bg-transparent "
-        font={paris}
-        novia="Guadalupe"
-        novio="Alejandro"
-        join="y"
-        color={{
-          bgColor: "bg-[#6b705c]",
-          titleColor: thirdColor,
-          textButtonColor: "text-white",
-        }}
-        showSobre={true}
-      /> */}
-      <div className={`${invitationbg}  h-screen `}>
-        <div className={` py-6 `}>
-          <Title />
-        </div>
-        <div>
-          <Datos />
+      <div
+        className={`${invitationbg}  h-screen flex flex-col justify-center items-center overflow-hidden relative`}
+      >
+        {/* Superior derecha */}
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          whileInView={{
+            scale: [0, 1.2, 1], // 👈 clave del pulso
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.6,
+            ease: "easeOut",
+          }}
+          viewport={{ once: true }}
+          className="absolute top-[-120px] right-[-106px] z-0"
+        >
+          <Image alt="disco" src={BolaDisco} />
+        </motion.div>
+        {/* Superior izquierda */}
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          whileInView={{
+            scale: [0, 1.2, 1], // 👈 clave del pulso
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.6,
+            ease: "easeOut",
+          }}
+          viewport={{ once: true }}
+          className="absolute top-[22px] left-[12px] z-0 w-16"
+        >
+          <Image alt="disco" src={BolaDisco} />
+        </motion.div>
+        {/* Inferior Izquierda */}
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          whileInView={{
+            scale: [0, 1.2, 1], // 👈 clave del pulso
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.6,
+            ease: "easeOut",
+          }}
+          viewport={{ once: true }}
+          className="absolute bottom-[-120px] left-[-106px] z-0"
+        >
+          <Image alt="disco" src={BolaDisco} />
+        </motion.div>
+        {/* Inferior izquierda 2*/}
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          whileInView={{
+            scale: [0, 1.2, 1], // 👈 clave del pulso
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.6,
+            ease: "easeOut",
+          }}
+          viewport={{ once: true }}
+          className="absolute bottom-[50px] left-[104px] z-0 w-[88px]"
+        >
+          <Image alt="disco" src={BolaDisco} />
+        </motion.div>
+        <div className="relative z-10">
+          <div className={` py-6 `}>
+            <Title />
+          </div>
+          <div>
+            <Datos />
+          </div>
         </div>
       </div>
 
-      <SongButtonPlayer2 backgroundColor="bg-[#d83a85]" />
+      {/* <SongButtonPlayer2 backgroundColor="bg-[#d83a85]" /> */}
     </InvitationContainer>
   );
 };
@@ -145,6 +192,11 @@ const Title = () => {
 const Datos = () => {
   return (
     <>
+      <div className="py-2">
+        <p className={`${RetroDolly.className} ${color1} text-center text-3xl`}>
+          Estas invitado a mi fiesta!
+        </p>
+      </div>
       <div className="py-2">
         <p
           className={`${ClassicDolly.className} ${color1} text-center text-3xl  underline`}
